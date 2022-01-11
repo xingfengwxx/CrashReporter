e4b287cde15c13f826406f64fd203a20892fc032
WangXingxing <1099420259@qq.com>
2022-01-11 16:24:42 +0800

    update

--- app/src/main/java/com/wangxingxing/crashreporter/App.kt
+++ app/src/main/java/com/wangxingxing/crashreporter/App.kt
@@ -1,6 +1,11 @@
 package com.wangxingxing.crashreporter
 
 import android.app.Application
+import android.os.Handler
+import android.os.Looper
+import android.os.Process
+import android.util.Log
+import kotlin.system.exitProcess
 
 /**
  * author : 王星星
@@ -10,9 +15,44 @@ import android.app.Application
  */
 class App : Application() {
 
+    private val TAG = "App"
+
     override fun onCreate() {
         super.onCreate()
 
-        CrashReport.init(applicationContext)
+//        CrashReport.init(applicationContext)
+
+        openCrashProtected()
+    }
+
+    private fun openCrashProtected() {
+        Log.d(TAG, "openCrashProtected")
+        Handler(Looper.getMainLooper()).post {
+            while (true) {
+                try {
+                    Looper.loop()
+                    Log.d(TAG, "main looper execute loop")
+                } catch (e: Throwable) {
+                    Log.e(TAG, "catch exception: " + e.toString())
+                    //主线程出现异常，关闭栈顶activity
+//                    ActivityStack.Instance().curr()?.finish()
+                }
+            }
+        }
+
+        Thread.setDefaultUncaughtExceptionHandler { _, e ->
+            //捕获到异常，只打印日志，不杀进程
+            Log.e(TAG, "${Thread.currentThread().name} 捕获到异常：$e")
+
+            //dump各个线程
+//            dumpAllThreadsInfo()
+
+            //执行完方法后，线程无法继续存活。
+            //如果是子线程，app不会crash。如果是主线程，就退出进程。
+            if (Looper.getMainLooper() == Looper.myLooper()) {
+                Process.killProcess(Process.myPid())
+                exitProcess(10)
+            }
+        }
     }
 }
